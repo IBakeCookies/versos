735fe4d70c686041c19567dd701d6331d4a7215b
Shadi <github-iro7msidew4z@revar.net>
2025-04-25 13:59:13 +0200

    added orders view

--- src/presentation/component/page/OrderPage.vue
+++ src/presentation/component/page/OrderPage.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import type { ComputedRef } from 'vue';
-import type { Product } from '@data/type/product';
+import type { Order } from '@data/type/order';
 import Button from '@component/Button.vue';
 import { useFetch } from '@vueuse/core';
 import { computed } from 'vue';
 
-const { isFetching, error, data } = useFetch('/api/orders').json<Record<'data', Product[]>>();
+const { isFetching, error, data } = useFetch('/api/orders').json<Record<'data', Order[]>>();
 
 const isZeroOrders: ComputedRef<boolean> = computed(() => {
     if (isFetching) {
@@ -14,11 +14,35 @@ const isZeroOrders: ComputedRef<boolean> = computed(() => {
 
     return data.value?.data.length === 0;
 });
+
+function onDelete(e: Event, productId: number): void {
+    e.preventDefault();
+
+    console.log('Order deleted:', productId);
+}
+
+function getTotalProductsQuantity(orderId: number): number {
+    if (!data.value) {
+        return 0;
+    }
+
+    return data.value.data.reduce((acc, order) => {
+        order.products.forEach((product) => {
+            if (order.id !== orderId) {
+                return acc;
+            }
+
+            acc += product.quantity;
+        });
+
+        return acc;
+    }, 0);
+}
 </script>
 
 <template>
     <!-- this part is kinda duplicated, should create a component like a small nav for this -->
-    <div class="p-4 bg-stone-100 rounded-xl flex items-center gap-4">
+    <div class="p-4 bg-stone-100 rounded-xl flex items-center gap-4 flex-wrap">
         <RouterLink to="/orders/create">
             <Button>Create order</Button>
         </RouterLink>
@@ -37,6 +61,33 @@ const isZeroOrders: ComputedRef<boolean> = computed(() => {
     <div v-if="isZeroOrders" class="mt-4">No orders found.</div>
 
     <ul class="mt-4 flex flex-col gap-4" v-if="!isFetching && data">
-        <!-- omitted - very similar to what we have in product -->
+        <!-- shoud be a component -->
+        <li
+            v-for="order in data.data"
+            :key="order.id"
+            class="flex items-center justify-between flex-wrap gap-4"
+        >
+            <div>
+                {{ order.customerCompanyId }} - {{ order.supplierCompanyId }} -
+                {{ order.shippingAddress }}
+
+                <div class="flex items-center gap-4">
+                    <b>Products count</b> {{ getTotalProductsQuantity(order.id) }}
+                    -
+                    <money><b>Total price:</b> {{ order.totalPrice }}$</money>
+                </div>
+            </div>
+
+            <div class="flex items-center gap-4">
+                <RouterLink :to="`/orders/edit/${order.id}`">
+                    <Button>Edit (Omitted)</Button>
+                </RouterLink>
+
+                <form @submit="(e) => onDelete(e, order.id)" method="POST">
+                    <input type="hidden" name="orderId" :value="order.id" />
+                    <Button variant="secondary" class="ml-4">&#x2715;</Button>
+                </form>
+            </div>
+        </li>
     </ul>
 </template>
